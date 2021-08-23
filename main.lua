a045dd00c3f5909087d06dcdb74b92c70c2859d6
eyup <eyupfindikli@gmail.com>
2021-08-23 22:19:36 +0300

    clipper

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
     require("lldebugger").start()
 end
 
+local clipper = require "clipper"
+path = clipper.Path(3) -- initialise vector of size 3
+path[0] = clipper.IntPoint(2, 3) -- specify first IntPoint in vector
+
 local mgl = require("MGL")
 local Camera = require("Camera")
 local camera = Camera:new()
